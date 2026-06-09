1455ca6775436282b4560097597accad2b54108f
rAel-Ael <alediru9267@gmail.com>
2026-06-09 17:21:42 +0200

    issue 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-
+import React, { useState } from 'react';
+import EventList from './components/EventList';
+import { events } from './data/events';
 
 function App() {
-  return;
+  const [filterCategory, setFilterCategory] = useState('Tutti');
+
+  const filteredEvents = events.filter(event => {
+
+    if (filterCategory === 'Tutti') {
+      return true;
+
+    }
+    return event.category === filterCategory;
+  });
+
+  return (
+    <div>
+      {/* header da mettere */}
+
+      {/* array filtro da mettere */}
+      <EventList events={filteredEvents} />
+    </div>
+  );
+
+
+
+
 }
 
 export default App
