fa6810bf2a5b741ca58b2ec24ef8031a18f2497a
Sara-Luongo <saraluongo202002@gmail.com>
2026-06-09 17:25:44 +0200

    modificato stile

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,23 @@ import './App.css'
 function App() {
   const [selectCategory, setSelectCategory] = useState('tutti')
 
-  return <div className='d-flex justify-content-center align-items-center' >
-    <label htmlFor="filter">
-      filtra Per Categoria
-    </label>
+  return <>
+    <div className='d-flex justify-content-center align-items-center mb-4'>
+      <label htmlFor="filter" className='me-3' >
+        filtra Per Categoria
+      </label>
 
-    <select value={selectCategory} id="filter" onChange={(e) => setSelectCategory(e.target.value)}>
-      <option value="Tutti">Tutti</option>
-      <option value="Conferenze">Conferenze</option>
-      <option value="Workshop">Workshop</option>
-      <option value="Meetup">Meetup</option>
-    </select>
-    <p>Categoria selezionata {selectCategory}</p>
-  </div>
+      <select value={selectCategory} id="filter" onChange={(e) => setSelectCategory(e.target.value)}>
+        <option value="Tutti">Tutti</option>
+        <option value="Conferenze">Conferenze</option>
+        <option value="Workshop">Workshop</option>
+        <option value="Meetup">Meetup</option>
+      </select>
 
+    </div>
+    <p className='text-center' mt-4>Categoria selezionata {selectCategory}</p>
 
-
+  </>
 
 
 
